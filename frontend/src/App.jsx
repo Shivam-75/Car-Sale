e5db3ef08096a5c8d225ed330a18fa801947a8a4
Shivam-75 <shivampandey7518200305@gmail.com>
2025-06-21 01:50:41 +0530

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import Nav from './header/nav'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './page/home'
-import "./css/home.css"
-import Service from './page/Service'
-import Login from './page/login'
-import Register from './page/Register'
-import Logout from './page/Logout'
-import Sales from './page/Sales'
-import Fotter from './page/foter'
-import Error from './error/Error'
-import Book from './page/Book'
+import React from "react";
+import Nav from "./header/nav";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./page/home";
+import "./css/home.css";
+import Service from "./page/Service";
+import Login from "./page/Login";
+import Register from "./page/Register";
+import Logout from "./page/Logout";
+import Sales from "./page/Sales";
+import Fotter from "./page/foter";
+import Error from "./error/Error";
 
 function App() {
   return (
@@ -20,15 +19,14 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Service" element={<Service />} />
         <Route path="/Login" element={<Login />} />
-        <Route path="/Register" element={<Register/>} />
-        <Route path="/Logout" element={<Logout/>} />
-        <Route path="/Sales" element={<Sales/>} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/Logout" element={<Logout />} />
+        <Route path="/Sales" element={<Sales />} />
         <Route path="*" element={<Error />} />
-        {/* <Route path='/Book' element={<Book/>}/> */}
       </Routes>
-      <Fotter/>
+      <Fotter />
     </BrowserRouter>
   );
 }
 
-export default App
+export default App;
